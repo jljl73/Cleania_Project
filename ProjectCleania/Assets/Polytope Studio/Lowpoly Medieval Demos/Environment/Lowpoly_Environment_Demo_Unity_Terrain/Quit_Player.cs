6d23ca858eefe13c07f7a5c4346b0eadf0f90717
jljl73 <jljl73@naver.com>
2021-09-07 13:31:19 +0900

    에셋 다운로드
    ㅇㅅㅇ

---
+++ ProjectCleania/Assets/Polytope Studio/Lowpoly Medieval Demos/Environment/Lowpoly_Environment_Demo_Unity_Terrain/Quit_Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit_Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
