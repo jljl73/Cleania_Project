8a3057288d9ceab006cfefd5b7761cdead557912
michaelkimm <alstjrdl951@gmail.com>
2021-09-08 11:20:27 +0900

    SkillManager & Movement merge
    - normal attack error: when i click close ground and click monster right away bug occur
    
    - when movement skill doesn't occur

--- ProjectCleania/Assets/Scripts/PlayerMovement.cs
+++ ProjectCleania/Assets/Scripts/PlayerMovement.cs
@@ -9,19 +9,14 @@ public class PlayerMovement : MonoBehaviour
 
     private Animator playerAnimator;            // 애니메이터 컴포넌트
     private NavMeshAgent playerNavMeshAgent;    // path 계산 컴포턴트
-    private Rigidbody playerRigidbody;          // 리지드바디 컴포넌트
 
     private BoxCollider attackBoxCollider;      // 공격 시 쓰이는 박스 콜라이더
 
     private GameObject targetObj;               // 공격 대상
     private Vector3 targetPose;                 // 목표 위치
 
-    private NavMeshPath path;                   // 목표까지 path
-    private int currentPathIdx;                 // path 내 현재 목표 지점
-
-    private float distanceBetweenTargetObj = 1f;   // 공격 시, 목표 위치 얼마 앞에서 멈출 것인가
-
-    private bool isAttackPlaying;
+    public float distanceBetweenTargetObj = 1f;   // 공격 시, 목표 위치 얼마 앞에서 멈출 것인가
+    private bool isAttackPlaying;                 // 공격 모션이 실행되고 있는가
 
     private void Awake()
     {
@@ -29,7 +24,6 @@ public class PlayerMovement : MonoBehaviour
         playerAnimator = GetComponent<Animator>();
         playerNavMeshAgent = GetComponent<NavMeshAgent>();
         attackBoxCollider = GetComponent<BoxCollider>();
-        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     void Start()
@@ -43,10 +37,16 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         // 공격 중이면 업데이트 x
-        if (!playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attack") && !playerAnimator.IsInTransition(0))
+        if (!playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Marry_Attack_01_h") && !playerAnimator.IsInTransition(0))
+        {
             isAttackPlaying = false;
+            playerNavMeshAgent.isStopped = false;
+        }
         if (isAttackPlaying)
+        {
+            playerNavMeshAgent.isStopped = true;
             return;
+        }
 
         // 마우스 클릭에 따른 네비게이션 실행
         ActivateNavigation();
@@ -70,7 +70,9 @@ public class PlayerMovement : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             // 클릭한 곳에 적이 있으면, 적 위치로 이동 후 공격
-            if (Physics.Raycast(ray, out mouseHit) && mouseHit.transform.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            //if (Physics.Raycast(ray, out mouseHit) && mouseHit.transform.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            int enemyLayerBitmask = 1 << LayerMask.NameToLayer("Enemy");
+            if (Physics.Raycast(ray, out mouseHit, 20f, enemyLayerBitmask))
             {
                 // 타겟: 마우스 클릭된 적
                 targetObj = mouseHit.transform.gameObject;
@@ -91,6 +93,7 @@ public class PlayerMovement : MonoBehaviour
             // 적이 바로 앞이면 움직일 필요x
             if (distanceBetweenTargetObj > Vector3.Distance(targetObj.transform.position, transform.position))
             {
+                print("enemy front!");
                 targetPose = transform.position;
                 return;
             }
@@ -117,17 +120,16 @@ public class PlayerMovement : MonoBehaviour
         rotateForward = Vector3.ProjectOnPlane(rotateForward, Vector3.up);
 
         // 회전
-
         transform.LookAt(this.transform.position + rotateForward);
     }
 
     private void Attack()
     {
-        // 타겟이 있고, 타겟 근처에 왔으면 공격 1회 실행
+        // <타겟이 있고, 타겟 근처에 있고, 타겟을 향했으면> 공격 1회 실행
         if (targetObj != null && Vector3.Distance(transform.position, targetPose) < 0.01f)
         {
             // 1회 공격 실시
-            playerAnimator.SetTrigger("Attack");
+            playerAnimator.SetTrigger("NormalAttack");
             isAttackPlaying = true;
 
             // 한번 공격 후 타겟 = null, 반복 실행 방지
