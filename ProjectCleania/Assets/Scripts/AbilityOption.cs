431519f0f7a5d5b3d8c9f5041a88e03666fd5182
Niiok <play.niiok@gmail.com>
2021-09-08 11:48:54 +0900

    option mod

--- ProjectCleania/Assets/Scripts/AbilityOption.cs
+++ ProjectCleania/Assets/Scripts/AbilityOption.cs
@@ -6,25 +6,26 @@ public class AbilityOption
 {
     public enum Name
     {
+        // Equipment
         Attack_Percent = 0,                 // 공격력 +n%
-        HeroMonsterDamage_Percent,      // 영웅 등급 이하 몬스터에게 주는 피해 +n%
-        RareMonsterDamage_Percent,      // 희귀 등급 이하 몬스터에게 주는 피해 +n%
-        BossMonsterDamage_Percent,      // 보스 등급 이하 몬스터에게 주는 피해 +n%
+        //HeroMonsterDamage_Percent,      // 영웅 등급 이하 몬스터에게 주는 피해 +n%
+        //RareMonsterDamage_Percent,      // 희귀 등급 이하 몬스터에게 주는 피해 +n%
+        //BossMonsterDamage_Percent,      // 보스 등급 이하 몬스터에게 주는 피해 +n%
         CriticalChance_Percent,         // 치명타 확률 +n%
         CriticalScale_Percent,          // 치명타 피해 +n%
-        IncreaseGivingDamage_Percent,   // 주는 피해 +n%
+        //IncreaseGivingDamage_Percent,   // 주는 피해 +n%
 
         Accuracy_Percent,               // 적중률 +n%
         Dodge_Percent,                  // 회피율 n%
         Toughness_Percent,              // 강인함 +n%
-        ReduceGettingDamage_Percent,    // 받는피해 -n%
+        //ReduceGettingDamage_Percent,    // 받는피해 -n%
 
         Defense_Abs,                    // 방어력 +n
         Vitality_Abs,                   // 체력 +n
         HP_Percent,                     // 생명력 + n%
-        HPperSecond_Abs,                // 초당 생명 회복 +n
-        HPperHit_Abs,                   // 타격당 생명 회복 +n
-        HPperKill_Abs,                  // 처치시 생명 회복 +n
+        //HPperSecond_Abs,                // 초당 생명 회복 +n
+        //HPperHit_Abs,                   // 타격당 생명 회복 +n
+        //HPperKill_Abs,                  // 처치시 생명 회복 +n
 
         Cooldown_Percent,               // 재사용대기시간 -n%
         MPconsumeReduce_Percent,        // 고유자원 소모량 -n%
@@ -33,13 +34,16 @@ public class AbilityOption
         EXP_Percent,                    // 적에게서 얻는 경험치
         Gold_Percent,                   // 적에게서 얻는 재화
 
-        EquipmentOptionTotal = 22,
 
-        MoveSpeed_Buff = 22,            // 버프 - 이동속도
+        // Buff
+        MoveSpeed_Buff,                 // 버프 - 이동속도
         AttackSpeed_Buff,               // 버프 - 공격속도
         Attack_Buff,                    // 버프 - 공격력
         Defense_Buff,                   // 버프 - 방어력
 
-        AllOptionTotal
+        AllOptionTotal,
+
+        EquipmentOptionTotal = Gold_Percent - Attack_Percent + 1,       // equip begin - equip end + 1
+        BuffOptionTotal = Defense_Buff - MoveSpeed_Buff + 1             // buff begin - buff end + 1
     }
 }
