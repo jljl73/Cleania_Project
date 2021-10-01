44037cb0bf3ca7829d1e34f6dba0c44ff5899759
Niiok <play.niiok@gmail.com>
2021-10-01 18:09:28 +0900

    error fix

--- ProjectCleania/Assets/Scripts/SinglePlayer.cs
+++ ProjectCleania/Assets/Scripts/SinglePlayer.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class SinglePlayer : MonoBehaviour
 {
-    void Start()
+    void Awake()
     {
         GameManager.Instance.SinglePlayer = this.gameObject;
         GameManager.Instance.PlayerAbility = GetComponent<AbilityStatus>();
         GameManager.Instance.PlayerStatus = GetComponent<Status>();
         GameManager.Instance.PlayerEquipments = GetComponent<EquipmentSlot>();
+        GameManager.Instance.PlayerBuffs = GetComponent<BuffManager>();
     }
 }
