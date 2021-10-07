2eb3a4d5e3faac2e92a27ae6d177101f15817894
michaelkimm <alstjrdl951@gmail.com>
2021-10-07 13:23:01 +0900

    Interactive static object
    Interactive static object prototype

--- ProjectCleania/Assets/Scripts/OnTriggerNameUI.cs
+++ ProjectCleania/Assets/Scripts/OnTriggerNameUI.cs
@@ -5,24 +5,13 @@ using UnityEngine.EventSystems;
 
 public class OnTriggerNameUI : NameUI
 {
-    private new void Awake()
-    {
-        base.Awake();
-        FloatContainer floatContainer = GetComponent<FloatContainer>();
-    }
-
-    void OnTriggerEnter(Collider other)
+    protected void OnTriggerEnter(Collider other)
     {
         base.ActiveUI(true);
         print("Trigger enter");
     }
 
-    private void OnTriggerStay(Collider other)
-    {
-        
-    }
-
-    void OnTriggerExit(Collider other)
+    protected void OnTriggerExit(Collider other)
     {
         base.ActiveUI(false);
         print("Trigger exit");
