257af6902a64113f50edb546784711639b482dab
Niiok <play.niiok@gmail.com>
2021-12-03 15:13:27 +0900

    HUD error fix

--- ProjectCleania/Assets/Scripts/Item_v2/UI_ItemController.cs
+++ ProjectCleania/Assets/Scripts/Item_v2/UI_ItemController.cs
@@ -242,6 +242,13 @@ public class UI_ItemController : MonoBehaviour,
     {
         if (eventData.button != PointerEventData.InputButton.Right) return;
 
+
+        if (currentContainer.SyncWith == UI_ItemContainer.SyncType.Equipment)
+            GameManager.Instance.uiManager.InventoryPanel.transform.SetAsLastSibling();
+        else
+            currentContainer.transform.SetAsLastSibling();
+
+
         switch (currentContainer.SyncWith)
         {
             case UI_ItemContainer.SyncType.Inventory:
