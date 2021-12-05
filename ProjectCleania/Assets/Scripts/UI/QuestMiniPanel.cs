0c30c1bd9ddeb6949730485b15bacb571e189d58
jljl73 <jljl73@naver.com>
2021-12-05 18:24:37 +0900

    퀘스트 패널 수정

--- ProjectCleania/Assets/Scripts/UI/QuestMiniPanel.cs
+++ ProjectCleania/Assets/Scripts/UI/QuestMiniPanel.cs
@@ -7,53 +7,47 @@ using UnityEngine.Events;
 
 public class QuestMiniPanel : MonoBehaviour
 {
-    //bool isOpend = false;
-    //[SerializeField]
-    //GameObject buttonOpen;
-    //[SerializeField]
-    //GameObject buttonClose;
+    QuestManager QM { get => GameManager.Instance.uiManager.GetComponent<QuestManager>(); }
 
-    //public QuestManager questManager;
-    //public GameObject Toggle;
-
-    //void Start()
-    //{
-        
-    //}
-
-    //public void OnClickedButton()
-    //{
-    //    OpenQuestList(isOpend);
-    //    isOpend = !isOpend;
-
-    //    buttonClose.SetActive(!isOpend);
-    //    buttonOpen.SetActive(isOpend);
-    //}
+    [SerializeField]
+    Transform miniLists;
     
-    //void OpenQuestList(bool value)
-    //{
-    //    int i = 2;
-    //    int count = transform.childCount;
-    //    for(; i < count; ++i)
-    //    {
-    //        transform.GetChild(i).gameObject.SetActive(value);
-    //    }
-    //}
-    
-    //void UpdateList()
-    //{
-    //    StringBuilder sb = new StringBuilder();
-    //    for (int q = 0; q < questManager.quests.Count; ++q)
-    //    {
-    //        sb.Clear();
-    //        sb.Append(questManager.quests[q].name);
-    //        sb.Append("\n");
-    //        for (int i = 0; i < questManager.quests[q].QuestNeeds.Length; ++i)
-    //        {
-    //            sb.Append(questManager.quests[q].QuestNeeds[i].Contents);
-    //            sb.Append("\n");
-    //        }
-    //        Toggle.transform.GetChild(q).GetChild(0).GetComponent<Text>().text = sb.ToString();
-    //    }
-    //}
+    void Start()
+    {
+        QM.AddListener_Update(UpdateList);
+    }
+
+
+    public void UpdateList()
+    {
+        if (miniLists == null) return;
+
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+
+        for (int q = 0; q < QM.Quests.Count; ++q)
+        {
+            if (QM.Quests[q].State != Quest.STATE.Assign && QM.Quests[q].State != Quest.STATE.Clear)
+                continue;
+
+            sb.Clear();
+            sb.Append(QM.Quests[q].Name);
+            sb.Append("\n");
+            miniLists.GetChild(index).gameObject.SetActive(true);
+            for (int i = 0; i < QM.Quests[q].QuestNeeds.Length; ++i)
+            {
+                sb.Append(QM.Quests[q].QuestNeeds[i].Contents);
+                sb.Append("\n");
+            }
+            miniLists.GetChild(index).GetChild(0).GetComponent<Text>().text = sb.ToString();
+
+            if (++index >= miniLists.childCount)
+                break;
+        }
+
+        for (; index < miniLists.childCount; ++index)
+        {
+            miniLists.GetChild(index).gameObject.SetActive(false);
+        }
+    }
 }
